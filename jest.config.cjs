2b2cfec5c63b3a7af52e862f0124ec27cccdbee5
AliAsjad512 <aa0504503@gmail.com>
2023-04-22 23:12:34 -0400

    unit test

---
+++ jest.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    testMatch: [
+      '**/__tests__/**/*.js?(x)',
+      '**/?(*.)+(spec|test).js?(x)'
+    ],
+    moduleFileExtensions: [
+      'js',
+      'json',
+      'jsx',
+      'node'
+    ],
+    moduleNameMapper: {
+      '\\.(css|less|scss)$': 'identity-obj-proxy'
+    },
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest'
+    }
+  };
+  
